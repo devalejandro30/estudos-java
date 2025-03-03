137a29194d721b7593fbfb981c91ee0482744243
DevJham <jhamalejandro23@gmail.com>
2025-03-03 10:29:25 -0300

    docs: estudando Arrays Multidimencionais

--- src/Arrays/ArraysMultidimencionais.java
+++ src/Arrays/ArraysMultidimencionais.java
@@ -3,8 +3,10 @@ package Arrays;
 public class ArraysMultidimencionais {
     public static void main(String[] args) {
         // array dentro de outro array
+        // funcionalidade? armazenar dados em várias dimensões,
+        // organizando-os em estruturas de matrizes
 
-        int[][] dias = new int[12][2];
+        int[][] dias = new int[3][3];
         dias[0][0] = 31;
         dias[0][1] = 28;
         dias[0][2] = 31;
@@ -14,8 +16,34 @@ public class ArraysMultidimencionais {
         dias[1][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("---------");
+
+        for (int[] arrayBase : dias) {
+            for (int num : arrayBase) {
+                System.out.println(num);
+            }
+        }
+
+        // variáveis de referência inicializam com null
+
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[]{1, 2, 3};
+        arrayInt[2] = new int[6];
+
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
+        // tamanho base do array [3] c/ 3 arrays 
+
+        for (int[] arrBase : arrayInt) {
+            System.out.println("\n-----");
+            for (int numeros : arrBase) {
+                System.out.print(numeros + " ");
             }
         }
     }
